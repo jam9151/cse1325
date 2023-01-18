f7cfab565b4b490d8fd79c86db33850cbe77909b
Jesse McNary <mcnaryjesse@gmail.com>
2023-01-17 18:49:47 -0600

    P01 bonus

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,16 @@
+//Jesse McNary
+import java.util.Scanner;
+
+
+
+public class Hello{  
+    public static void main(String[] args){  
+	        //System.out.println("Hello Jesse!")
+			Scanner scanObj = new Scanner(System.in);
+			System.out.println("Please Enter Name");
+			String name = scanObj.nextLine();
+			System.out.println("Hello " + name + "!");
+
+
+			    }  
+		}  
